09d97b9233621c91e2638b071b3247f81d83cf82
Joey Vagedes <joeyvagedes@microsoft.com>
2025-11-17 09:07:40 -0800

    Reference implementation of static core

--- bin/q35_dxe_core.rs
+++ bin/q35_dxe_core.rs
@@ -13,8 +13,8 @@
 use core::{ffi::c_void, panic::PanicInfo};
 use patina::{log::Format, serial::uart::Uart16550};
 use patina_adv_logger::{component::AdvancedLoggerComponent, logger::AdvancedLogger};
-use patina_dxe_core::Core;
-use patina_samples::component as sc;
+use patina_dxe_core::*;
+use patina_ffs_extractors::CompositeSectionExtractor;
 use patina_stacktrace::StackTrace;
 use qemu_resources::q35::component::service as q35_services;
 extern crate alloc;
@@ -58,60 +58,45 @@ static DEBUGGER: patina_debugger::PatinaDebugger<Uart16550> =
         .with_force_enable(_ENABLE_DEBUGGER)
         .with_log_policy(patina_debugger::DebuggerLoggingPolicy::FullLogging);
 
+struct Q35;
+
+impl ComponentInfo for Q35 {
+    fn components(mut add: Add<Component>) {
+        add.component(AdvancedLoggerComponent::<Uart16550>::new(&LOGGER));
+        add.component(q35_services::mm_config_provider::MmConfigurationProvider);
+        add.component(q35_services::mm_control::QemuQ35PlatformMmControl::new());
+        add.component(patina_mm::component::sw_mmi_manager::SwMmiManager::new());
+    }
+
+    fn configs(mut add: Add<Config>) {
+        add.config(patina_mm::config::MmCommunicationConfiguration {
+            acpi_base: patina_mm::config::AcpiBase::Mmio(0x0), // Actual ACPI base address will be set during boot
+            cmd_port: patina_mm::config::MmiPort::Smi(0xB2),
+            data_port: patina_mm::config::MmiPort::Smi(0xB3),
+            comm_buffers: vec![],
+        });
+    }
+}
+
+impl Platform for Q35 {
+    type ComponentInfo = Self;
+    type Extractor = CompositeSectionExtractor;
+}
+
+static CORE: Core<Q35> = Core::new(CompositeSectionExtractor::new());
+
 #[cfg_attr(target_os = "uefi", unsafe(export_name = "efi_main"))]
 pub extern "efiapi" fn _start(physical_hob_list: *const c_void) -> ! {
-    log::set_logger(&LOGGER).map(|()| log::set_max_level(log::LevelFilter::Trace)).unwrap();
-    let adv_logger_component = AdvancedLoggerComponent::<Uart16550>::new(&LOGGER);
     // SAFETY: The physical_hob_list pointer is considered valid at this point as it's provided by the core
     // to the entry point.
     unsafe {
-        adv_logger_component.init_advanced_logger(physical_hob_list).unwrap();
+        LOGGER.init_advanced_logger(physical_hob_list).unwrap();
     }
+    log::set_logger(&LOGGER).map(|()| log::set_max_level(log::LevelFilter::Trace)).unwrap();
 
     #[cfg(feature = "build_debugger")]
     patina_debugger::set_debugger(&DEBUGGER);
 
     log::info!("DXE Core Platform Binary v{}", env!("CARGO_PKG_VERSION"));
-
-    Core::default()
-        .init_memory(physical_hob_list) // We can make allocations now!
-        .with_service(patina_ffs_extractors::CompositeSectionExtractor::default())
-        .with_component(adv_logger_component)
-        .with_component(sc::hello_world::HelloStruct("World")) // Example of a struct component
-        .with_component(sc::hello_world::GreetingsEnum::Hello("World")) // Example of a struct component (enum)
-        .with_component(sc::hello_world::GreetingsEnum::Goodbye("World")) // Example of a struct component (enum)0
-        .with_config(patina_mm::config::MmCommunicationConfiguration {
-            acpi_base: patina_mm::config::AcpiBase::Mmio(0x0), // Actual ACPI base address will be set during boot
-            cmd_port: patina_mm::config::MmiPort::Smi(0xB2),
-            data_port: patina_mm::config::MmiPort::Smi(0xB3),
-            comm_buffers: vec![],
-        })
-        .with_component(q35_services::mm_config_provider::MmConfigurationProvider)
-        .with_component(q35_services::mm_control::QemuQ35PlatformMmControl::new())
-        .with_component(patina_mm::component::sw_mmi_manager::SwMmiManager::new())
-        // The Q35 firmware using the MM Supervisor. Additional support is needeed in the
-        // supervisor to support MM communication outside of code that has direct access
-        // to C internal state.
-        //
-        // Tracked in https://github.com/microsoft/mu_feature_mm_supv/issues/541
-        //
-        // .with_component(patina_mm::component::communicator::MmCommunicator::new())
-        // .with_component(q35_services::mm_test::QemuQ35MmTest::new())
-        .with_config(patina_performance::config::PerfConfig {
-            enable_component: true,
-            enabled_measurements: {
-                patina::performance::Measurement::DriverBindingStart         // Adds driver binding start measurements.
-               | patina::performance::Measurement::DriverBindingStop        // Adds driver binding stop measurements.
-               | patina::performance::Measurement::DriverBindingSupport     // Adds driver binding support measurements.
-               | patina::performance::Measurement::LoadImage                // Adds load image measurements.
-               | patina::performance::Measurement::StartImage // Adds start image measurements.
-            },
-        })
-        .with_component(patina_performance::component::performance_config_provider::PerformanceConfigurationProvider)
-        .with_component(patina_performance::component::performance::Performance)
-        .start()
-        .unwrap();
-
-    log::info!("Dead Loop Time");
-    loop {}
+    CORE.entry_point(physical_hob_list)
 }
