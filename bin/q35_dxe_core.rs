c0f36e9f5d6c9ef02a843ad78955360c1c2f0bf1
Oliver Smith-Denny <osde@microsoft.com>
2025-12-04 11:40:30 -0800

    Run patina_tests by Default on Q35 and SBSA
    The patina test feature was enabled on these platforms,
    but the test runner component wasn't added, so tests weren't
    actually getting executed. This adds the test runner to run
    all tests. Eventually, this may be filtered.
    
    This also updates Cargo.lock to pull in Patina 17.1.0 which
    contains new patina_tests.
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

--- bin/q35_dxe_core.rs
+++ bin/q35_dxe_core.rs
@@ -111,6 +111,9 @@ impl ComponentInfo for Q35 {
         add.component(q35_services::mm_test::QemuQ35MmTest::new());
         add.component(patina_performance::component::performance_config_provider::PerformanceConfigurationProvider);
         add.component(patina_performance::component::performance::Performance);
+        add.component(patina::test::TestRunner::default().with_callback(|test_name, err_msg| {
+            log::error!("Test {} failed: {}", test_name, err_msg);
+        }));
     }
 }
 
